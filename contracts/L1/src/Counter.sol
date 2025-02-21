f1d3fd86f7c4a946d6eafb6afe88960ae2c3c4a4
enyinnaya1234 <chemicalnilton@gmail.com>
2025-02-21 15:35:23 +0100

    Feat: Implement whitelist feature for token security

--- contracts/L1/src/Counter.sol
+++ contracts/L1/src/Counter.sol
@@ -3,7 +3,24 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    address public admin;
 
+       // Events
+    event WhitelistEvent(address indexed token);
+    event DewhitelistEvent(address indexed token);
+
+    mapping (address => bool) public whitelistedTokens;
+
+    constructor(){
+        admin = address(0x123);
+        //admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can perform this action");
+    
+    _;
+    }
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
@@ -11,4 +28,17 @@ contract Counter {
     function increment() public {
         number++;
     }
+
+    function whitelistToken(address _token) public onlyAdmin {
+        whitelistedTokens[_token] = true;
+        emit WhitelistEvent(_token);
+    }
+
+    function dewhitelistToken(address _token) public onlyAdmin {
+        whitelistedTokens[_token] = false;
+        emit DewhitelistEvent(_token);
+    }
+    function isWhitelisted(address _token) public view returns (bool) {
+    return whitelistedTokens[_token];
+}
 }
--- contracts/L1/test/Counter.t.sol
+++ contracts/L1/test/Counter.t.sol
@@ -1,15 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "../lib/forge-std/src/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
+    address admin;
+    address token1;
+    address token2;
 
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+        admin = address(0x123);
+        token1 = address(0x456);
+        token2 = address(0x789);
     }
 
     function test_Increment() public {
@@ -20,5 +26,60 @@ contract CounterTest is Test {
     function testFuzz_SetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
+    }
+
+    function testWhitelistToken() public {
+        // Whitelist token1
+        vm.prank(admin);
+
+        vm.expectEmit(true, true, false, false);
+        emit Counter.WhitelistEvent(token1);
+        counter.whitelistToken(token1); 
+
+        // Check if token1 is whitelisted
+        assertTrue(counter.isWhitelisted(token1), "Token1 should be whitelisted");
+
+        // Check the storage variable directly
+        assertTrue(counter.whitelistedTokens(token1), "Token should be whitelisted in storage");
+    }
+
+    function testDewhitelistToken() public {
+        // Whitelist token1 first
+        vm.prank(admin);
+        counter.whitelistToken(token1);
+
+        // Now dewhitelist token1
+        vm.prank(admin);
+        
+        vm.expectEmit(true, true, false, false);
+        emit Counter.DewhitelistEvent(token1);
+
+        counter.dewhitelistToken(token1);
+
+        // Check if token1 is dewhitelisted
+        assertFalse(counter.isWhitelisted(token1), "Token1 should be dewhitelisted");
+        assertFalse(counter.whitelistedTokens(token1), "Token1 should be dewhitelisted in storage");
+    }
+
+    function testOnlyAdminCanWhitelist() public {
+        address nonAdmin = address(0x999);
+
+        vm.startPrank(nonAdmin);
+        vm.expectRevert("Only admin can perform this action");
+        counter.whitelistToken(token1);
+        vm.stopPrank();
+    }
+
+    function testOnlyAdminCanDewhitelist() public {
+        // Whitelist token1 first
+        vm.prank(admin);
+        counter.whitelistToken(token1);
+
+        address nonAdmin = address(0x999);
+
+        vm.startPrank(nonAdmin);
+        vm.expectRevert("Only admin can perform this action");
+        counter.dewhitelistToken(token1);
+        vm.stopPrank();
     }
 }
